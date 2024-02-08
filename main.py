fc3f12213c503927f4291f1f75acec485264e83d
Ehsan Eslamian <eeslamian@gmail.com>
2024-02-08 12:39:34 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 from flask import Flask, render_template, request, send_file
 import subprocess
 import os
+import logging
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.ERROR)
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return "No file part"
+try:
+    @app.route("/")
+    def index():
+        return render_template('index.html')
 
-    file = request.files['file']
-    if file.filename == '':
-        return "No selected file"
+    @app.route('/upload', methods=['POST'])
+    def upload():
+        if 'file' not in request.files:
+            return "No file part"
 
-    file.save('uploads/' + 'sipoc_table.csv')  # Save the uploaded file
-    print("Debugging: Reached line 21")
+        file = request.files['file']
+        if file.filename == '':
+            return "No selected file"
 
-    # Call your Python script with the uploaded file as an argument
-    subprocess.run(['python', 'sipoc-to-pptx-4-Flask.py', 'uploads/' + file.filename])
-    print("Debugging: Reached line 25")
+        file.save('uploads/' + 'sipoc_table.csv')  # Save the uploaded file
+        print("Debugging: Reached line 21")
+        # Call your Python script with the uploaded file as an argument
+        subprocess.run(['python', 'sipoc-to-pptx-4-Flask.py', 'uploads/' + file.filename])
+        print("Debugging: Reached line 25")
+        return "File uploaded and processed successfully"
 
+except Exception as e:
+    # Log the error message
+    logging.error(f'An error occurred: {str(e)}')
 
-    return "File uploaded and processed successfully"
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
