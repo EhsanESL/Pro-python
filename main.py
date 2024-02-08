69a4022dbf95774679a5c1e6181e378064e908eb
Ehsan Eslamian <eeslamian@gmail.com>
2024-02-08 21:47:38 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -8,20 +8,23 @@ app = Flask(__name__)
 # Configure logging
 logging.basicConfig(filename='app.log', level=logging.INFO)
 
+# Define upload status constants
+UPLOAD_SUCCESS = "File uploaded and processed successfully"
+NO_FILE_SELECTED = "No file selected"
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', upload_status="")
 
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
         if 'file' not in request.files:
-            return "No file part"
+            return render_template('index.html', upload_status=NO_FILE_SELECTED)
 
         file = request.files['file']
         if file.filename == '':
-            return "No selected file"
+            return render_template('index.html', upload_status=NO_FILE_SELECTED)
 
         file.save('uploads/' + 'sipoc_table.csv')  # Save the uploaded file
         logging.info("CSV file saved successfully.")
@@ -30,11 +33,11 @@ def upload():
         subprocess.run(['python3', 'sipoc-to-pptx-4-Flask.py', 'uploads/' + file.filename])
         logging.info("Python script executed successfully.")
 
-        return "File uploaded and processed successfully"
+        return render_template('index.html', upload_status=UPLOAD_SUCCESS)
     except Exception as e:
         # Log any exceptions that occur
         logging.error(f'An error occurred: {str(e)}')
-        return "An error occurred while processing the file.."
+        return render_template('index.html', upload_status="An error occurred while processing the file..")
 
 
 if __name__ == '__main__':
