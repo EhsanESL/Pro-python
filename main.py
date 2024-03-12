caf961ea3a713c9097b2c8d2fe3f2a6ecb661872
Ehsan Eslamian <eeslamian@gmail.com>
2024-03-12 15:19:59 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, send_file, jsonify, session
+from flask import Flask, render_template, request, send_file, jsonify, session, make_response
 import subprocess
 import os
 import logging
 import uuid  # for generating unique identifiers
+import zipfile
 
 
 
@@ -57,14 +58,14 @@ def upload():
         logging.info(f"Filename without extension: {filename_without_extension}")
 
         # Call your Python3 scripts with the uploaded file as an argument
-        logging.info("calling your Python3 scripts.")
-        subprocess.run(['python3', 'sipoc-to-pptx-4-Flask.py', file_path])
+        logging.info("calling your Python scripts.")
+        subprocess.run(['python', 'sipoc-to-pptx-4-Flask.py', file_path])
         logging.info("sipoc-to-pptx-4-Flask.py executed successfully.")
-        subprocess.run(['python3', 'Seperate_verbs.py', file_path])
+        subprocess.run(['python', 'Seperate_verbs.py', file_path])
         logging.info("Seperate_verbs.py executed successfully.")
-        subprocess.run(['python3', 'Bracket.py', file_path])
+        subprocess.run(['python', 'Bracket.py', file_path])
         logging.info("Bracket.py executed successfully.")
-        subprocess.run(['python3', 'Decision-Bubbles.py', file_path])
+        subprocess.run(['python', 'Decision-Bubbles.py', file_path])
         logging.info("Decision-Bubbles.py executed successfully.")
 
         return jsonify({'message': 'Upload successful'}), 200
@@ -72,6 +73,70 @@ def upload():
         # Log any exceptions that occur
         logging.error(f'An error occurred: {str(e)}')
         return jsonify({'error': 'An error occurred while processing the file'}), 500
+
+
+
+
+@app.route('/download_all_files')
+def download_all_files():
+    try:
+        file_path = session.get('file_path')
+        filename_with_identifier = os.path.basename(file_path)
+        filename_without_extension = os.path.splitext(filename_with_identifier)[0]
+
+        logging.info("files_to_download")
+        pptx=os.path.join('uploads', filename_without_extension +'.pptx')
+        logging.info({pptx})
+
+        verbs_pptx=os.path.join('uploads',filename_without_extension +'_verbs'+'.pptx')
+        logging.info({verbs_pptx})
+
+        decision_bubbles=os.path.join('uploads', filename_without_extension + '_decision-bubbles' + '.pptx')
+        logging.info({decision_bubbles})
+        
+        subbubbles=os.path.join('uploads',filename_without_extension +'_subbubbles'+'.pptx')
+        logging.info({subbubbles})
+
+                
+        
+        # Specify the paths to the required files
+        
+        files_to_download = [
+            pptx,
+            verbs_pptx,
+            decision_bubbles,
+            subbubbles
+            
+        ]
+        logging.info(f"all files address:{files_to_download}")
+
+        # Check if all files exist
+        for file in files_to_download:
+            if not os.path.exists(file):
+                return "One or more files are missing"
+        
+        logging.info("All required files existttttttt.")
+
+        # Create a ZIP file containing all the required files
+        zip_filename = os.path.join('uploads', filename_without_extension + 'zip')
+        with zipfile.ZipFile(zip_filename, 'w') as zipf:
+            for file in files_to_download:
+                zipf.write(file, os.path.basename(file))# Create a ZIP file containing all the required files in the 'uploads' folder
+
+        logging.info("All files zipped successfully.")
+
+        # Return the ZIP file for download
+        response = make_response(send_file(zip_filename, as_attachment=True))
+        response.headers['Content-Disposition'] = 'attachment; filename=all_files.zip'
+        response.headers['Content-Type'] = 'application/zip'
+        logging.info(f"ZIP file for download: {response}")
+        return response
+
+    except Exception as e:
+        # Log any exceptions that occur
+        logging.error(f'An error occurred: {str(e)}')
+        return "An error occurred while downloading the files."
+
 
 
 @app.route('/download_pptx')
@@ -105,12 +170,12 @@ def download_verbs():
         if file_path:
             filename_with_identifier = os.path.basename(file_path)
             filename_without_extension = os.path.splitext(filename_with_identifier)[0]
-            verbs_csvfile= os.path.join('uploads',filename_without_extension +'verbs'+'.csv')
-            logging.info("Verbs csv file downloaded successfully.")
+            verbs_pptxfile= os.path.join('uploads',filename_without_extension +'_verbs'+'.pptx')
+            logging.info("Verbs pptx file downloaded successfully.")
 
 
         # Return the file as an attachment
-        return send_file(verbs_csvfile, as_attachment=True)
+        return send_file(verbs_pptxfile, as_attachment=True)
     except Exception as e:
         # Log any exceptions that occur
         logging.error(f'An error occurred: {str(e)}')
@@ -148,7 +213,7 @@ def download_Sub_bubbles():
         if file_path:
             filename_with_identifier = os.path.basename(file_path)
             filename_without_extension = os.path.splitext(filename_with_identifier)[0]
-            verbs_pptxfile= os.path.join('uploads',filename_without_extension +'_SubBubbles'+'.pptx')
+            verbs_pptxfile= os.path.join('uploads',filename_without_extension +'_subbubbles'+'.pptx')
             logging.info(f"Sub_bubbles pptx file path {verbs_pptxfile}")
             logging.info("Sub_bubbles pptx file downloaded successfully.")
 
